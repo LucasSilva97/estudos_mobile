2d319cf62a9d37b1e627a5af76168a2ddde0857d
LucasSilva97 <lucas-silva-santana@hotmail.com>
2026-09-09 12:37:04 -0300

    Aula 1

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,4 +1,20 @@
 
+import 'dart:io';
+
 void main() {
-  print("Hello World");
+  print("Hello World"); //Exibe mensagem
+  stdin.readLineSync(); // Para receber mensagens no terminal (função semelhante ao input() do python)
+  // Observação: praticamente toda função do projeto terá um bloco de parênteses.
+
+  // Declarando uma variável
+  var entrada = "Uma mensagem";
+  print(entrada);
 }
+
+// Anot importante: o Dart, para não ficar pesado, foi dividido em várias partes
+// para manter suas várias funcionalidades, usando de bom desempenho
+
+// Dart Core - possui as principais funções, como a main() e a print()
+// dart:io - Usa comandos de IN-OUT (entrada e saída)
+
+
