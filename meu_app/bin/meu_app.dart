eda1e3136f4691ad7e138ee1001deea5fbce71bc
LucasSilva97 <lucas-silva-santana@hotmail.com>
2026-09-09 14:07:48 -0300

    Primeiro programa dart

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,15 +1,14 @@
 
 import 'dart:io';
 
-void main() {
-  print("Hello World"); //Exibe mensagem
-  stdin.readLineSync(); // Para receber mensagens no terminal (função semelhante ao input() do python)
+ /* print("Hello World"); //Exibe mensagem
+  // stdin.readLineSync(); // Para receber mensagens no terminal (função semelhante ao input() do python)
   // Observação: praticamente toda função do projeto terá um bloco de parênteses.
 
   // Declarando uma variável
-  var entrada = "Uma mensagem";
+  // var entrada = "Uma mensagem";
+  var entrada = stdin.readLineSync(); // Dessa forma a variável recebe um valor de entrada do usuário, igual ao input do python
   print(entrada);
-}
 
 // Anot importante: o Dart, para não ficar pesado, foi dividido em várias partes
 // para manter suas várias funcionalidades, usando de bom desempenho
@@ -17,4 +16,15 @@ void main() {
 // Dart Core - possui as principais funções, como a main() e a print()
 // dart:io - Usa comandos de IN-OUT (entrada e saída)
 
+*/
 
+// Reescrevendo o programa
+
+void main() {
+  
+  print("Olá, me chamo Dart. Qual o seu nome?");
+  var entrada = stdin.readLineSync();
+  // Usando interpolação
+  print("Muito prazer, $entrada. Vamos fazer vários programas juntos!");
+
+}
