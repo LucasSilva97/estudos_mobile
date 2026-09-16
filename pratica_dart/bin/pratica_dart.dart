55b0493634e3dc7ead19fc308ed8ccc5662ddf7e
LucasSilva97 <lucas-silva-santana@hotmail.com>
2026-09-16 14:51:24 -0300

    Iniciando prática em dart

---
+++ pratica_dart/bin/pratica_dart.dart
@@ -0,0 +1,5 @@
+import 'package:pratica_dart/pratica_dart.dart' as pratica_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pratica_dart.calculate()}!');
+}
---
+++ pratica_dart/test/pratica_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:pratica_dart/pratica_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
