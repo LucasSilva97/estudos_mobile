c8f5d19b9a05f321b386c88ac6c1fe8c3cfd762a
LucasSilva97 <lucas-silva-santana@hotmail.com>
2026-09-22 18:03:19 -0300

    Aula 07: Funções - Módulo 02 - em andamento

--- pratica_dart/bin/aula07_funcoes.dart
+++ pratica_dart/bin/aula07_funcoes.dart
@@ -7,6 +7,10 @@ typedef Validador = String? Function(String entrada);
 /// 1. Parâmetros posicionais obrigatórios - usando arrow function
 String formatar(String materia, int minutos) => '$materia: $minutos min';
 
+/// 2. Parâmetro posicional opcional com valor padrão
+String registrar(String materia, [int minutos = 25]) => '$materia por $minutos min';
+
 void main(){
   print(formatar('Flutter', 90));
+  print(registrar('Dart'));
 }
