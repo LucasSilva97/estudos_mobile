26b31c3ae638871af3badc1c5b3eda4c342253c6
LucasSilva97 <lucas-silva-santana@hotmail.com>
2026-09-24 22:48:42 -0300

    Treinando condicionais com calculadora

---
+++ curso_alura/bin/aula03_tratando_condicionais/calculadora.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+void main() {
+  double numeroUm = double.parse(stdin.readLineSync()!);
+  double numeroDois = double.parse(stdin.readLineSync()!);
+  String operacao = stdin.readLineSync()!;
+
+
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao() {
+    print(numeroUm - numeroDois);
+  }
+
+  void divisao() {
+    print(numeroUm / numeroDois);
+  }
+
+  void multiplicacao() {
+    print(numeroUm * numeroDois);
+  }
+
+
+/*
+  if(operacao == '+'){
+    soma();
+  } else if (operacao == '-') {
+    subtracao();
+  } else if (operacao == '*') {
+    multiplicacao();
+  } else if (operacao == '/'){
+    divisao();
+  } else {
+    print('Operação inválida!');
+  }
+*/
+
+// Usando switch case
+
+  switch(operacao){
+    case '+': soma();
+    case '-': subtracao();
+    case '*': multiplicacao();
+    case '/': divisao();
+    default: print('Operação inválida!');
+  }
+}
