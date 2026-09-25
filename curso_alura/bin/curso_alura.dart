38b3e59ea1ae6c095ebd284adcf127867077b16b
LucasSilva97 <lucas-silva-santana@hotmail.com>
2026-09-24 22:20:23 -0300

    Adicionando material da alura

---
+++ curso_alura/bin/curso_alura.dart
@@ -0,0 +1,5 @@
+import 'package:curso_alura/curso_alura.dart' as curso_alura;
+
+void main(List<String> arguments) {
+  print('Hello world: ${curso_alura.calculate()}!');
+}
---
+++ curso_alura/test/curso_alura_test.dart
@@ -0,0 +1,8 @@
+import 'package:curso_alura/curso_alura.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
